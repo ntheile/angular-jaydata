3e30426c6d83abbb29deab332c54f29d17bf2190
Nathan Shepperd <nas@scumble.net>
2013-12-24 09:53:35 +0000

    Initial commit to repo

--- js/controllers.js
+++ js/controllers.js
@@ -6,22 +6,6 @@ var skillsApp = angular.module('skillsApp', ['jaydata']);
 
 skillsApp.controller('SkillsListController', function ($scope, $data) {
 
-    $scope.someData =
-        [
-            {
-                skill: 'Turnip eating',
-                rating: 5
-            },
-            {
-                skill: 'Ferret chasing',
-                rating: 3
-            },
-            {
-                skill: 'Forest clearing',
-                rating: 12
-            }
-        ];
-
     $scope.SkillRatings = [];
 
     $data.initService('http://localhost:52999').then(function (service) {
